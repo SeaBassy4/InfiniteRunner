0f2b58d1c911575bf07aa5c5520b7cc44d51ab0f
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-05-23 13:00:58 -0700

    death handling and leaderboard

--- infinite-runner/gamemanager.gd
+++ infinite-runner/gamemanager.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 signal ScoreUpdated(score : int)
+signal GameOver(score : int)
+signal StartGame()
 var score : int = 0
 
 @export var ObstacleScene : Array[PackedScene] = []
@@ -10,7 +12,9 @@ var score : int = 0
 @export var SpawnDistance : float = -20.0
 @export var PickupableSpawnChance : float = .3
 
+
 var lanePositions = [-2.0, 0.0, 2.0]
+var speedMultiplyer : float = 1.0
 
 func _on_spawn_timer_timeout() -> void:
 	
@@ -32,6 +36,7 @@ func _on_spawn_timer_timeout() -> void:
 			var obstacle = obstacleScene.instantiate()
 			obstacle.position = Vector3(0, 0, SpawnDistance)
 			$ObstacleContainer.add_child(obstacle)
+			obstacle.Speed = obstacle.Speed * speedMultiplyer
 			currentOB.queue_free()
 		else:
 			var openLane = randi() % 3
@@ -40,18 +45,21 @@ func _on_spawn_timer_timeout() -> void:
 					var obstacle = obstacleScene.instantiate()
 					obstacle.position = Vector3(lanePositions[i], 0, SpawnDistance)
 					$ObstacleContainer.add_child(obstacle)
+					obstacle.Speed = obstacle.Speed * speedMultiplyer
 			currentOB.queue_free()
 	pass 
 
 
 func _on_score_timer_timeout() -> void:
-	score += 1
+	score += 1 + (1 * speedMultiplyer)
+	speedMultiplyer += 0.01
 	ScoreUpdated.emit(score)
 	pass 
 
 
 func _on_player_add_score(score: int) -> void:
-	self.score += score
+	self.score += score + (score * speedMultiplyer)
+	speedMultiplyer += 0.01
 	ScoreUpdated.emit(self.score)
 	pass 
 
@@ -63,11 +71,13 @@ func spawnCoinFormation(coinScene : PackedScene):
 			var coin = coinScene.instantiate()
 			coin.position = Vector3(lanePositions[lane], 0, SpawnDistance)
 			$ObstacleContainer.add_child(coin)
+			coin.Speed = coin.Speed * speedMultiplyer
 		1:
-			for i in range(randi() % 7):
+			for i in range(randi() % 3):
 				var coin = coinScene.instantiate()
 				coin.position = Vector3(lanePositions[lane], 0, SpawnDistance + (i *2))
 				$ObstacleContainer.add_child(coin)
+				coin.Speed = coin.Speed * speedMultiplyer
 		2: 
 			var direction = 1 if randf() > 0.5 else -1
 			if lane < 0:
@@ -77,3 +87,20 @@ func spawnCoinFormation(coinScene : PackedScene):
 				var coin = coinScene.instantiate()
 				coin.position = Vector3(lanePositions[index], 0, SpawnDistance + (i *2))
 				$ObstacleContainer.add_child(coin)
+				coin.Speed = coin.Speed * speedMultiplyer
+
+
+func _on_death_plane_area_entered(area: Area3D) -> void:
+	$SpawnTimer.stop()
+	$ScoreTimer.stop()
+	GameOver.emit(score)
+	pass
+
+func _on_ui_manager_start_game() -> void:
+	score = 0
+	speedMultiplyer = 1.0
+	$ScoreTimer.start()
+	$SpawnTimer.start()
+	ScoreUpdated.emit(score)
+	StartGame.emit()
+	pass
--- infinite-runner/ui_manager.gd
+++ infinite-runner/ui_manager.gd
@@ -1,7 +1,45 @@
 extends Control
 
+signal StartGame
 
+var highScores : Array = []
+@export var MaxLeaderBoardEntries : int = 10
 
 func _on_game_manager_score_updated(score: int) -> void:
 	$RichTextLabel.text = "Score: " + str(score)
+	pass # Replace with function body.
+
+
+func _on_game_manager_game_over(score: int) -> void:
+	$GameOverContainer.show()
+	$GameOverContainer/VBoxContainer/ScoreLabel.text = "Score: " + str(score)
+	
+	$LeaderBoard.show()
+	highScores.append(score)
+	highScores.sort()
+	highScores.reverse()
+	
+	if highScores.size() > MaxLeaderBoardEntries: 
+		highScores.resize(MaxLeaderBoardEntries)
+		
+	for child in $LeaderBoard/VBoxContainer.get_children():
+		if child.name != "Label":
+			child.queue_free()
+	
+	for i in range(highScores.size()):
+		var entry = RichTextLabel.new()
+		entry.text = "%d. %d" % [i + 1, highScores[i]]
+		$LeaderBoard/VBoxContainer.add_child(entry)
+		entry.bbcode_enabled = true
+		entry.custom_minimum_size = Vector2(0,25)
+		if i == 0:
+			entry.text = "[b]%d. %d" % [i + 1, highScores[i]]
+	pass 
+
+
+func _on_button_button_down() -> void:
+	StartGame.emit()
+	$GameOverContainer.hide()
+	$StartGameContainer.hide()
+	$LeaderBoard.hide()
 	pass # Replace with function body.
