2cc284c5824c43f13b3d7cdfb1b57d232ef08df6
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-05-22 18:05:05 -0700

    Pickups + coin pickup signal

---
+++ infinite-runner/Pickup.gd
@@ -0,0 +1,15 @@
+extends Area3D
+class_name Pickup
+
+enum PickupType {COIN, POWERUP, BOOST}
+
+@export var Speed : float = 10.0
+@export var Score = 1
+
+func _process(delta: float) -> void:
+	position.z += Speed * delta
+
+
+func _on_area_entered(area: Area3D) -> void:
+	queue_free()
+	pass # Replace with function body.
--- infinite-runner/gamemanager.gd
+++ infinite-runner/gamemanager.gd
@@ -4,33 +4,50 @@ signal ScoreUpdated(score : int)
 var score : int = 0
 
 @export var ObstacleScene : Array[PackedScene] = []
+@export var PowerupsScene : Array[PackedScene] = []
 @export var MinSpawnTime : float = 1.0
 @export var MaxSpawnTime : float = 2.0
 @export var SpawnDistance : float = -20.0
+@export var PickupableSpawnChance : float = .3
 
 var lanePositions = [-2.0, 0.0, 2.0]
 
 func _on_spawn_timer_timeout() -> void:
-	var obstacleScene = ObstacleScene[randi() % ObstacleScene.size()]
-	var currentOB = obstacleScene.instantiate()
 	
-	if currentOB.CurrentObstacleType == Obstacle.ObstacleType.LOW or currentOB.CurrentObstacleType == Obstacle.ObstacleType.HIGH: 
-		var obstacle = obstacleScene.instantiate()
-		obstacle.position = Vector3(0, 0, SpawnDistance)
-		$ObstacleContainer.add_child(obstacle)
-		currentOB.queue_free()
+	var randValue = randf()
+	if randValue < PickupableSpawnChance:
+		var obstacleScene = PowerupsScene[randi() % PowerupsScene.size()]
+		var currentOB = obstacleScene.instantiate()
+		var spawnedLane = randi() % 3
+		currentOB.position = Vector3(lanePositions[spawnedLane], 0, SpawnDistance)
+		$ObstacleContainer.add_child(currentOB)
 	else:
-		var openLane = randi() % 3
-		for i in range(3):
-			if i != openLane:
-				var obstacle = obstacleScene.instantiate()
-				obstacle.position = Vector3(lanePositions[i], 0, SpawnDistance)
-				$ObstacleContainer.add_child(obstacle)
-		currentOB.queue_free()
+		var obstacleScene = ObstacleScene[randi() % ObstacleScene.size()]
+		var currentOB = obstacleScene.instantiate()
+		
+		if currentOB.CurrentObstacleType == Obstacle.ObstacleType.LOW or currentOB.CurrentObstacleType == Obstacle.ObstacleType.HIGH: 
+			var obstacle = obstacleScene.instantiate()
+			obstacle.position = Vector3(0, 0, SpawnDistance)
+			$ObstacleContainer.add_child(obstacle)
+			currentOB.queue_free()
+		else:
+			var openLane = randi() % 3
+			for i in range(3):
+				if i != openLane:
+					var obstacle = obstacleScene.instantiate()
+					obstacle.position = Vector3(lanePositions[i], 0, SpawnDistance)
+					$ObstacleContainer.add_child(obstacle)
+			currentOB.queue_free()
 	pass 
 
 
 func _on_score_timer_timeout() -> void:
 	score += 1
 	ScoreUpdated.emit(score)
+	pass 
+
+
+func _on_player_add_score(score: int) -> void:
+	self.score += score
+	ScoreUpdated.emit(self.score)
 	pass 
